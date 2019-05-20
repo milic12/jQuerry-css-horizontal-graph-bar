05f640be071df25ce956412e578d5afdab9998f8
Manojlo Ilić <50624481+milic12@users.noreply.github.com>
2019-05-20 11:42:46 +0200

    Update jquery.js

--- js/jquery.js
+++ js/jquery.js
@@ -1,1 +1,19 @@
+jQuery(document).ready(function(){
+	
+	function checkAnimation() {
+    var $elem = $('.bar .level');
 
+    if (isElementInViewport($elem)) {
+        // Start the animation
+        $elem.addClass('start');
+    } else {
+        $elem.removeClass('start');
+    }
+}
+	
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},9000);
+	});
+});
